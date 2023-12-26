a09bce280ee6a9677f7f25b19a4752150071001f
Andreaj42 <andrea.joly1989@gmail.com>
2023-12-26 18:13:39 +0100

    remove warning : player may be null

--- src/test/java/org/rygn/tse_springboot/PlayerControllerTest.java
+++ src/test/java/org/rygn/tse_springboot/PlayerControllerTest.java
@@ -33,122 +33,119 @@ public class PlayerControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@Autowired
 	private PlayerRepository playerRepository;
-	
+
 	@Test
-	public void testAllPlayers()  throws Exception{
-		
+	public void testAllPlayers() throws Exception {
+
 		this.mockMvc
-			.perform(get("/players"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.length()", is(2)));
+				.perform(get("/players"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.length()", is(2)));
 	}
-	
+
 	@Test
-	public void testOnePlayer()  throws Exception{
-		
+	public void testOnePlayer() throws Exception {
+
 		this.mockMvc
-			.perform(get("/players/1"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.name", is("Christiano Ronaldo")));
+				.perform(get("/players/1"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.name", is("Christiano Ronaldo")));
 	}
-	
+
 	@Test
-	public void testCreatePlayer()  throws Exception{
-		
+	public void testCreatePlayer() throws Exception {
+
 		Player player = new Player();
 		player.setName("Lionel Messi");
-		
+
 		ObjectMapper mapper = new ObjectMapper();
-        byte[] playerAsBytes = mapper.writeValueAsBytes(player);
-		
-		
+		byte[] playerAsBytes = mapper.writeValueAsBytes(player);
+
 		this.mockMvc
-			.perform(
-					post("/players")
-					.accept(MediaType.APPLICATION_JSON)
-					.contentType(MediaType.APPLICATION_JSON).content(playerAsBytes))
-			.andExpect(status().isOk())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.name", is("Lionel Messi")));
-		
+				.perform(
+						post("/players")
+								.accept(MediaType.APPLICATION_JSON)
+								.contentType(MediaType.APPLICATION_JSON).content(playerAsBytes))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.name", is("Lionel Messi")));
+
 		assertEquals(4, this.playerRepository.count());
-		
+
 		Collection<Player> players = playerRepository.findAll();
-		
+
 		boolean found = false;
-		
+
 		for (Player currentPlayer : players) {
-			
+
 			if (currentPlayer.getName().equals("Lionel Messi")) {
-				
+
 				found = true;
-				
+
 				playerRepository.delete(currentPlayer);
 			}
 		}
-		
+
 		assertTrue(found);
 	}
-	
+
 	@Test
 	public void testDeletePlayer() throws Exception {
-		
+
 		Player player = new Player();
 		player.setName("Kylian Mbappe");
-		
+
 		this.playerRepository.save(player);
-		
+
 		Collection<Player> players = this.playerRepository.findAll();
-		
+
 		Long id = 0L;
-		
+
 		for (Player currentPlayer : players) {
-			
+
 			if (currentPlayer.getName().equals("Kylian Mbappe")) {
-				
+
 				id = currentPlayer.getId();
 			}
 		}
-		
+
 		this.mockMvc.perform(
-					delete("/players/" + id)
-					.accept(MediaType.APPLICATION_JSON))
-					.andExpect(status().isOk());
-		
+				delete("/players/" + id)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+
 		assertEquals(2, this.playerRepository.count());
 	}
-	
+
 	@Test
 	public void testReplacePlayer() throws Exception {
-		
+
 		Player player = new Player();
 		player.setName("Hugo Lloris");
-		
+
 		ObjectMapper mapper = new ObjectMapper();
-        byte[] playerAsBytes = mapper.writeValueAsBytes(player);
-        
-        this.mockMvc.perform(
-						put("/players/1")
+		byte[] playerAsBytes = mapper.writeValueAsBytes(player);
+
+		this.mockMvc.perform(
+				put("/players/1")
 						.accept(MediaType.APPLICATION_JSON)
 						.contentType(MediaType.APPLICATION_JSON).content(playerAsBytes))
-						.andExpect(status().isOk());
-        
-        player = this.playerRepository.findById(1L).orElse(null);
-        
-        if (player == null) {
-        	
-        	fail("Player not found");
-        }
-        
-        assertEquals("Hugo Lloris", player.getName());
-        
-        player.setName("Kylian Mbappe");
-        
-        this.playerRepository.save(player);
+				.andExpect(status().isOk());
+
+		player = this.playerRepository.findById(1L).orElse(null);
+
+		if (player != null) {
+			assertEquals("Hugo Lloris", player.getName());
+			player.setName("Kylian Mbappe");
+			this.playerRepository.save(player);
+		}
+		else{
+			fail("Player not found");
+		}
 	}
 }
