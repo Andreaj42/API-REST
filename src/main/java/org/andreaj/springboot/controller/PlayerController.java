d457d34a59674dcce595bc74a28b32be3bce718d
Andreaj42 <andrea.joly1989@gmail.com>
2023-12-28 14:53:15 +0100

    remove useless spaces

--- src/main/java/org/andreaj/springboot/controller/PlayerController.java
+++ src/main/java/org/andreaj/springboot/controller/PlayerController.java
@@ -15,47 +15,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlayerController {
-	
-	
+
 	@Autowired
 	private PlayerService playerService;
-	
+
 	@GetMapping("/players")
 	public List<Player> allPlayers() {
-				
+
 		return this.playerService.findAllPlayers();
 	}
-	
+
 	@GetMapping("/players/{id}")
 	public Player onePlayer(@PathVariable Long id) {
-				
+
 		return this.playerService.findPlayer(id);
 	}
-	
+
 	@PostMapping("/players")
 	public Player createPlayer(@RequestBody Player player) {
-		
+
 		return this.playerService.createPlayer(player);
 	}
-	
+
 	@DeleteMapping("/players/{id}")
 	public void deletePlayer(@PathVariable Long id) {
-		
+
 		this.playerService.deletePlayer(id);
 	}
-	
+
 	@PutMapping("/players/{id}")
 	Player replacePlayer(@RequestBody Player player, @PathVariable Long id) {
 
 		Player foundPlayer = this.playerService.findPlayer(id);
-		
+
 		if (player != null) {
-			
+
 			foundPlayer.setName(player.getName());
-			
+
 			foundPlayer = this.playerService.savePlayer(foundPlayer);
 		}
-		
+
 		return foundPlayer;
 	}
 }
